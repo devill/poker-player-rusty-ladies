934320639c55ebb4dfc4b35efc04c55247aeadfd
Ivett Ördög <devillmeister@gmail.com>
2024-05-28 12:04:31 +0200

    Fix bluff logic

--- Player.js
+++ Player.js
@@ -31,7 +31,7 @@ class Player {
       let bothHigh = lowestCard  > 10;
       let suited = holeCards[0].suit === holeCards[1].suit;
       let otherAllIn = gameState.players.filter(player => player.stack === 0 && player.status === 'active').length;
-      if( otherAllIn === 0 && ( player.dealer + 2 ) % 3 === player.in_action) {
+      if( otherAllIn === 0 ) {
         bet(10000);
         return;
       }
