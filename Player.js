c2461eb6eab0b07575841a4b4e6219b1b9d4e2e3
Ivett Ördög <devillmeister@gmail.com>
2024-05-28 11:34:24 +0200

    Adjust agressiveness based on number of players

--- Player.js
+++ Player.js
@@ -26,7 +26,12 @@ class Player {
       holeCards[0].value = Player.toValue(holeCards[0].rank);
       holeCards[1].value = Player.toValue(holeCards[1].rank);
       let highestCard = Math.max(holeCards[0].value, holeCards[1].value);
-      bet(isPair && highestCard > 10 ? 10000 : 0);
+      let countOutPlayers = gameState.players.filter(player => player.status === 'out').length;
+      if(countOutPlayers === 1) {
+        bet(isPair && (highestCard > 8) ? 10000 : 0);
+      } else {
+        bet(isPair && (highestCard > 12) ? 10000 : 0);
+      }
     } catch (e) {
       console.error(e);
       bet(0);
