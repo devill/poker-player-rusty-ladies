edec3f711c23c4fd46e0ed7193b98cbb88eee62f
Ivett Ördög <devillmeister@gmail.com>
2024-05-28 14:25:15 +0200

    removed positoning

--- Player.js
+++ Player.js
@@ -32,7 +32,8 @@ class Player {
       let bothHigh = lowestCard  > 10;
       let suited = holeCards[0].suit === holeCards[1].suit;
       let otherAllIn = gameState.players.filter(player => player.stack < player.bet && player.status === 'active').length;
-      if( otherAllIn === 0 && lastToAct) {
+      //if( otherAllIn === 0 && lastToAct) {
+      if( otherAllIn === 0) {
         bet(10000);
         return;
       }
